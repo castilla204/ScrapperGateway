7ca6837cd675643e8a648cb927ad455bdbd75a22
castilla204 <tu_email@ejemplo.com>
2025-04-20 19:34:40 +0200

    subbiendo

--- ServicesLayer/Web3Service.cs
+++ ServicesLayer/Web3Service.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using DataLayer;
+using Microsoft.Extensions.Logging;
 
 namespace ServicesLayer
 {
@@ -7,15 +8,19 @@ namespace ServicesLayer
     {
         private readonly IWeb3Data _web3Data;
         private readonly IMapper _mapper;
+        private readonly ILogger<Web3Service> _logger;
 
-        public Web3Service(IMapper mapper)
+        public Web3Service(IWeb3Data web3Data, IMapper mapper, ILogger<Web3Service> logger)
         {
+            _web3Data = web3Data;
             _mapper = mapper;
-            _web3Data = new Web3Data(_mapper);
+            _logger = logger;
+            _logger.LogInformation("Web3Service inicializado correctamente.");
         }
 
         public async Task<string> GetWallapop(string keywords, int pagestoscrap, int? category, string? latitude, string? longitude, int? minprice, int? maxprice, bool shippingAviable, bool isProgrammed)
         {
+            _logger.LogInformation("Llamando a Web3Data.SearchWallapop con palabras clave: {Keywords}", keywords);
             return await _web3Data.SearchWallapop(keywords, pagestoscrap, category, latitude, longitude, minprice, maxprice, shippingAviable, isProgrammed);
         }
     }
